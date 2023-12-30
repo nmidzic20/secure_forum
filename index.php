4a9ae149cc47c15f792e22ed732d18f060fb1f9c
Marko Novak <marko.novak.2468@gmail.com>
2023-12-30 15:33:28 +0100

    Created simple frontend for testing purposes

---
+++ index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+	<header>
+		<form id="loginForm" style="float: right;" method="POST" action="/login.php">
+			<input type="text" placeholder="username" />
+			<input type="password" placeholder="********" />
+			<input type="submit" value="login" />
+		</form>
+	</header>
+	<nav>
+		Navigation, navigation on the wall, tell me who is the laziest of all
+	</nav>
+	<br>
+	<section>
+		<div class="thread">
+			<div id="somethingrandomorsomethingidk" class="message">
+				<details open="">
+					<summary>
+						Post title and/or author
+					</summary>
+					<div>
+						Post content
+					</div>
+					<blockquote id="randID1" class="reply">
+						<!-- replies -->
+
+						<details open="">
+							<summary>
+								Reply title and/or author
+							</summary>
+							<div>
+								The reply
+							</div>
+							<blockquote id="randID2" class="reply">
+							<!-- replies -->
+
+								<details open="">
+									<summary>
+										John McJohnFace
+									</summary>
+									<div>
+										Get free Robux at <a href="https://www.duck.com">fake link</a> :D
+									</div>
+									<blockquote id="randID3" class="reply">
+									<!-- replies -->
+
+									</blockquote>
+								</details>
+
+								<details open="">
+									<summary>
+										John McJohnFace2
+									</summary>
+									<div>
+										Download Minecraft for free at <a href="https://www.duck.com">fake link</a> :D
+									</div>
+									<blockquote id="randID4" class="reply">
+									<!-- replies -->
+
+									</blockquote>
+								</details>
+
+							</blockquote>
+						</details>
+
+						<details open="">
+							<summary>
+								Steve McStevenson
+							</summary>
+							<div>
+								Heed my warning. The moon is made of cheese.
+							</div>
+							<blockquote id="randID5" class="reply">
+							<!-- replies -->
+
+							</blockquote>
+						</details>
+
+					</blockquote>
+				</details>
+
+				</div>
+			</div>
+	</section>
+	<footer>
+		&copy; SIS 2024.
+	</footer>
+</body>
+</html>
