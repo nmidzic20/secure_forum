7ee433409090aae3fc0473ba44936450c2b94afd
Noa Midzic <nmidzic@yahoo.com>
2024-01-01 16:38:33 +0100

    Add login page

--- index.php
+++ index.php
@@ -8,93 +8,93 @@
 </head>
 <body>
 	<header>
-		<form id="loginForm" style="float: right;" method="POST" action="/login.php">
-			<input type="text" placeholder="username" />
-			<input type="password" placeholder="********" />
-			<input type="submit" value="login" />
-		</form>
+		<nav class="navbar">
+			<a class="active-page" href="">Home</a>
+			<a href="html/profile.html">Profile</a>
+			<a href="html/login.html">Login</a>
+			<a href="html/registration.html">Registration</a>
+		</nav>
+		<title>Home</title>
 	</header>
-	<nav class="navbar">
-		<a href="home.html">Home</a>
-		<a href="profile.html">Profile</a>
-		<a href="login.html">Login</a>
-		<a href="registration.html">Registration</a>
-	</nav>
-	<br>
-	<section>
-		<div class="thread">
-			<div id="somethingrandomorsomethingidk" class="message">
-				<details open="">
-					<summary>
-						Post title and/or author
-					</summary>
-					<div>
-						Post content
+	<main>
+		<section>
+			<div class="thread">
+				<div id="somethingrandomorsomethingidk" class="message">
+					<details open="">
+						<summary>
+							Post title and/or author
+						</summary>
+						<div>
+							Post content
+						</div>
+						<blockquote id="randID1" class="reply">
+							<!-- replies -->
+
+							<details open="">
+								<summary>
+									Reply title and/or author
+								</summary>
+								<div>
+									The reply
+								</div>
+								<blockquote id="randID2" class="reply">
+								<!-- replies -->
+
+									<details open="">
+										<summary>
+											John McJohnFace
+										</summary>
+										<div>
+											Get free Robux at <a href="https://www.duck.com">fake link</a> :D
+										</div>
+										<blockquote id="randID3" class="reply">
+										<!-- replies -->
+
+										</blockquote>
+									</details>
+
+									<details open="">
+										<summary>
+											John McJohnFace2
+										</summary>
+										<div>
+											Download Minecraft for free at <a href="https://www.duck.com">fake link</a> :D
+										</div>
+										<blockquote id="randID4" class="reply">
+										<!-- replies -->
+
+										</blockquote>
+									</details>
+
+								</blockquote>
+							</details>
+
+							<details open="">
+								<summary>
+									Steve McStevenson
+								</summary>
+								<div>
+									Heed my warning. The moon is made of cheese.
+								</div>
+								<blockquote id="randID5" class="reply">
+								<!-- replies -->
+
+								</blockquote>
+							</details>
+
+						</blockquote>
+					</details>
+
 					</div>
-					<blockquote id="randID1" class="reply">
-						<!-- replies -->
-
-						<details open="">
-							<summary>
-								Reply title and/or author
-							</summary>
-							<div>
-								The reply
-							</div>
-							<blockquote id="randID2" class="reply">
-							<!-- replies -->
-
-								<details open="">
-									<summary>
-										John McJohnFace
-									</summary>
-									<div>
-										Get free Robux at <a href="https://www.duck.com">fake link</a> :D
-									</div>
-									<blockquote id="randID3" class="reply">
-									<!-- replies -->
-
-									</blockquote>
-								</details>
-
-								<details open="">
-									<summary>
-										John McJohnFace2
-									</summary>
-									<div>
-										Download Minecraft for free at <a href="https://www.duck.com">fake link</a> :D
-									</div>
-									<blockquote id="randID4" class="reply">
-									<!-- replies -->
-
-									</blockquote>
-								</details>
-
-							</blockquote>
-						</details>
-
-						<details open="">
-							<summary>
-								Steve McStevenson
-							</summary>
-							<div>
-								Heed my warning. The moon is made of cheese.
-							</div>
-							<blockquote id="randID5" class="reply">
-							<!-- replies -->
-
-							</blockquote>
-						</details>
-
-					</blockquote>
-				</details>
-
 				</div>
-			</div>
-	</section>
+		</section>
+	</main>
 	<footer>
-		&copy; SIS 2024.
-		<a href="https://www.flaticon.com/free-icons/bulletin" title="bulletin icons">Bulletin icons created by Alfredo Creates - Flaticon</a>
+		<div>&copy; SIS 2024.</div>
+		<div>
+			<img src="./assets/favicon.png" height="15">
+			<a href="https://www.flaticon.com/free-icons/bulletin" title="bulletin icons">Bulletin icons created by Alfredo Creates - Flaticon</a>
+		</div>
 	</footer>
 </body>
 </html>
