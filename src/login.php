9d5775796ab551789c8ebebf0cc3f5b125bc8f5f
StjepanPetrovic <stjepanpetrovic11@gmail.com>
2024-01-02 19:01:02 +0100

    Refactore logic for login to use database

--- src/login.php
+++ src/login.php
@@ -2,22 +2,34 @@
 
 session_start();
 
-$users = array(
-    array("username" => "user1", "password" => "pass1"),
-    array("username" => "user2", "password" => "pass2"),
-);
+$config = include('../database/db_config.php');
 
-$username = $_POST['username'];
-$password = $_POST['password'];
+$host = $config['host'];
+$db   = $config['db'];
+$user = $config['user'];
+$pass = $config['pass'];
 
-foreach ($users as $user) {
-    if ($user['username'] == $username && $user['password'] == $password) {
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $stmt = $pdo->prepare("SELECT * FROM user WHERE username = :username AND password = :password");
+    $stmt->execute(['username' => $username, 'password' => $password]);
+    $user = $stmt->fetch();
+
+    if ($user) {
         $_SESSION['loggedin'] = true;
 
         header("Location: ../index.php");
 
         exit;
+    } else {
+        echo "Login failed";
     }
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
 }
-
-echo "Login failed";
