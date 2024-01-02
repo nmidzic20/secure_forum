9882ae282eb855de3b709ce73ed74548933accaa
Noa Midzic <nmidzic@yahoo.com>
2024-01-02 10:42:24 +0100

    Add styling for Home

--- index.php
+++ index.php
@@ -5,13 +5,16 @@
 	<link rel="stylesheet" type="text/css" href="./css/style.css" />
 	<link rel="icon" href="./assets/favicon.ico" type="image/x-icon" />
 	<link rel="shortcut icon" href="./assets/favicon.ico" type="image/x-icon" />
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;400;500;600;700&family=Poppins:wght@200&family=Ranchers&display=swap" rel="stylesheet">
 </head>
 <body>
 	<header>
 		<nav class="navbar">
 			<a class="active-page" href="">Home</a>
 			<a href="html/profile.html">Profile</a>
-			<a href="html/login.html">Login</a>
+			<a href="html/login.html">Log In</a>
 			<a href="html/registration.html">Registration</a>
 		</nav>
 		<title>Home</title>
@@ -49,7 +52,7 @@
 										</div>
 										<blockquote id="randID3" class="reply">
 										<!-- replies -->
-
+											<a class="see-replies" href="">See replies</a>
 										</blockquote>
 									</details>
 
@@ -62,7 +65,7 @@
 										</div>
 										<blockquote id="randID4" class="reply">
 										<!-- replies -->
-
+											<a class="see-replies" href="">See replies</a>
 										</blockquote>
 									</details>
 
@@ -96,5 +99,11 @@
 			<a href="https://www.flaticon.com/free-icons/bulletin" title="bulletin icons">Bulletin icons created by Alfredo Creates - Flaticon</a>
 		</div>
 	</footer>
+	<!--<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+	<script>
+		$('.see-replies').parent('blockquote').css({
+			"background-color": "aliceblue"
+		});
+	</script>-->
 </body>
 </html>
