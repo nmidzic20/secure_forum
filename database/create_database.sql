b11a23ca13d57ad67aa109fc832a857aa28cb716
Noa Midzic <nmidzic@yahoo.com>
2024-01-03 16:35:17 +0100

    Create Dockerfile and docker-compose.yml, change create_database.php to create_database.sql

---
+++ database/create_database.sql
@@ -0,0 +1,26 @@
+-- Create database if not exists
+CREATE DATABASE IF NOT EXISTS secure_forum;
+
+-- Use the secure_forum database
+USE secure_forum;
+
+-- Create user table
+CREATE TABLE IF NOT EXISTS user (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(30) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    date_of_registration TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Create topic table
+CREATE TABLE IF NOT EXISTS topic (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
+    file_path VARCHAR(255)
+);
+
+INSERT INTO user (username, email, password) 
+VALUES ('user', 'user@gmail.com', '123456');
+
